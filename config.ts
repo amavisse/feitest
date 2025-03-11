d6206763a234b54cd406741c0d92a6a08a3fb1ce
amavisse <110661617+amavisse@users.noreply.github.com>
2025-03-11 12:37:44 +0000

    initial commit

---
+++ config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { cleanEnv, str, url } from "envalid";
+
+const env = cleanEnv(process.env, {
+  SHOPIFY_STORE_URL: url(),
+  SHOPIFY_ADMIN_API_ACCESS_TOKEN: str(),
+});
+
+export const SHOPIFY_STORE_URL = env.SHOPIFY_STORE_URL;
+export const SHOPIFY_ADMIN_API_ACCESS_TOKEN = env.SHOPIFY_ADMIN_API_ACCESS_TOKEN;
+export const ADMIN_GRAPHQL_ENDPOINT = `${SHOPIFY_STORE_URL}/api/2025-01/graphql.json`;
