930ad437bf04e148841ffdd730b0e2d8396933c0
amavisse <110661617+amavisse@users.noreply.github.com>
2025-03-11 17:23:51 +0000

    fix typo

--- config.ts
+++ config.ts
@@ -8,4 +8,4 @@ const env = cleanEnv(process.env, {
 
 export const SHOPIFY_STORE_URL = env.SHOPIFY_STORE_URL;
 export const SHOPIFY_ADMIN_API_ACCESS_TOKEN = env.SHOPIFY_ADMIN_API_ACCESS_TOKEN;
-export const ADMIN_GRAPHQL_ENDPOINT = `${SHOPIFY_STORE_URL}/api/2025-01/graphql.json`;
+export const ADMIN_GRAPHQL_ENDPOINT = `${SHOPIFY_STORE_URL}/admin/api/2025-01/graphql.json`;
