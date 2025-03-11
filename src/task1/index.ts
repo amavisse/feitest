0e7069b313b595406210a880db6f0ecd8ed2a572
amavisse <110661617+amavisse@users.noreply.github.com>
2025-03-11 17:43:56 +0000

    wrap in promise and export as default

--- src/task1/index.ts
+++ src/task1/index.ts
@@ -1,15 +1,18 @@
 import { spawn } from "child_process";
 
-export function runPlaywrightTests() {
-  const playwrightProcess = spawn("npx", ["playwright", "test", "--headed"], {
-    stdio: "inherit",
-    shell: true,
-  });
-  playwrightProcess.on("close", (code) => {
-    if (code === 0) {
-      console.log("Playwright tests completed successfully.");
-    } else {
-      console.error(`Playwright tests failed with code ${code}`);
-    }
+export default async function runPlaywrightTests() {
+  return new Promise((resolve, reject) => {
+    const playwrightProcess = spawn("npx", ["playwright", "test", "--headed"], {
+      stdio: "inherit",
+      shell: true,
+    });
+    playwrightProcess.on("close", (code) => {
+      if (code === 0) {
+        console.log("Playwright tests completed successfully.");
+      } else {
+        console.error(`Playwright tests failed with code ${code}`);
+      }
+      resolve(() => {});
+    });
   });
 }
